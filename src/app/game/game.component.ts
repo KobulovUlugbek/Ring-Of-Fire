d18dff12eaf40e067e5aa1a4c5284c16dbe7074f
Ulugbek Kobulov <108155304+KobulovUlugbek@users.noreply.github.com>
2023-07-29 15:13:28 +0200

    First playable version

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -29,8 +29,10 @@ export class GameComponent implements OnInit {
     if (!this.pickCardAnimation) {
       this.currentCard = this.game.stack.pop();
       this.pickCardAnimation = true;
-      console.log(this.game);
       
+      this.game.currentPlayer ++;
+      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+
       setTimeout(() => {
         this.game.playedCards.push(this.currentCard);
         this.pickCardAnimation = false;
@@ -42,7 +44,9 @@ export class GameComponent implements OnInit {
     const dialogRef = this.dialog.open(DialogAddPlayerComponent);
 
     dialogRef.afterClosed().subscribe((name: string) => {
+      if(name && name.length > 0){
       this.game.players.push(name)
-    });
+    }
+  });
   }
 }
